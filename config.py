3eb115319c79693c59435a92242e5f50c11b1e0a
Chan Jian Long <141404237+Jianlong09@users.noreply.github.com>
2024-08-30 15:40:01 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.cfwqfbxhphml.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "chanjianlong-employee"
+customregion = "us-east-1"
+
